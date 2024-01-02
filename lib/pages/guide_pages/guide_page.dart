84753b02da3f887822c67918501fcc58ec2fac38
yutokobayashii <kmyhm93161@gmail.com>
2024-01-02 17:57:39 +0900

    Revert "ガイド修正"
    This reverts commit 59f7e2f4e983ede2919cf35bf6db8bd5ed3b1f70.

--- lib/pages/guide_pages/guide_page.dart
+++ lib/pages/guide_pages/guide_page.dart
@@ -1,57 +1,73 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:share_question/constant/color_constant.dart';
-import 'package:share_question/pages/guide_pages/guide_widget/guide_page_widget.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../constant/color_constant.dart';
+import '../../provider/shared_prefrence_provider.dart';
 
-
-
-class GuideWidget extends StatelessWidget {
-  const GuideWidget({
-    super.key,
-    required this.title,
-  });
-
-  final String title;
+class GuidePage extends HookConsumerWidget {
+  const GuidePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pop(context);
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => GuidePageWidget(title:title)),
-        );
-      },
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        color: Colors.white,
-        child: Column(
-          children: [
-             SizedBox(height: 30.h,),
+  Widget build(BuildContext context,WidgetRef ref) {
+    final currentPage = useState(0);
+    final PageController pageController = PageController();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: GestureDetector(
+              onTap: () {
+               Navigator.pop(context);
+              },
+              child: const Icon(
+                Icons.arrow_back,
+                size: 28,
+              )
+          ),
+          title: const Text('ガイド'),
 
-            Container(
-              width: MediaQuery.of(context).size.width - 50.w,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1,
+
+        ),
+        body: Container(
+          color: Colors.white,
+          child:  Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  controller: pageController,
+                  onPageChanged: (int page) {
+                    currentPage.value = page;
+                  },
+                  children: [
+                    Container(color: Colors.red),
+                    Container(color: Colors.green),
+                    Container(color: Colors.blue),
+                  ],
                 ),
               ),
-
-              child: Center(child: Text(title,style: boldTextStyle,)),
-            )
-
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  3, // ページ数
+                      (index) => Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 10,
+                    height: 10,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: currentPage.value == index
+                          ? Color(ref.watch(colorSharedPreferencesProvider).getInt("color") ?? baseColor.value) // 現在のページ
+                          : Colors.grey, // 他のページ
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
--- lib/pages/settings_pages/settings_page.dart
+++ lib/pages/settings_pages/settings_page.dart
@@ -4,12 +4,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:share_question/constant/color_constant.dart';
 import 'package:share_question/pages/settings_pages/setting_widgets/kiyaku.page.dart';
 import 'package:share_question/pages/settings_pages/setting_widgets/privacy_policy_page.dart';
-import 'package:share_question/pages/settings_pages/setting_widgets/setting_widget.dart';
 import 'package:share_question/provider/shared_prefrence_provider.dart';
 
-import '../../controller/setting_controller/setting_controller.dart';
-import '../guide_pages/guide_widget/select_guide_widget.dart';
-
+import '../guide_pages/guide_page.dart';
 
 final colorProvider = StateProvider<int>((ref) => ref.watch(colorSharedPreferencesProvider).getInt("color") ?? baseColor.value);
 
@@ -19,10 +16,7 @@ class SettingsPage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context,WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text("設定"),
-          backgroundColor: Colors.white,
-      ),
+      appBar: AppBar(title: const Text("設定")),
       body: Padding(
         padding: const EdgeInsets.only(left: 15),
         child: Container(
@@ -76,16 +70,9 @@ class SettingsPage extends HookConsumerWidget {
               SettingWidget(
                 title: '使い方',
                 action: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => const GuidePage()),
-                  // );
-                  showModalBottomSheet(
-                    context: context,
-                    isScrollControlled: true, // 画面半分よりも大きなモーダルの表示設定
-                    builder: (BuildContext context) {
-                      return const SelectGuideWidget();
-                    },
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const GuidePage()),
                   );
                 },),
 
@@ -118,11 +105,90 @@ class SettingsPage extends HookConsumerWidget {
   }
 }
 
+class SettingWidget extends StatelessWidget {
+  const SettingWidget({
+    super.key,
+    required this.title,
+    required this.action,
+  });
+
+  final String title;
+  final VoidCallback action;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        width: MediaQuery.of(context).size.width - 50.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey,
+            width: 1,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title,style: normalTextStyle,),
+            const Icon(Icons.chevron_right),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 
 
 
 
 
+void showColorPalette(BuildContext context,WidgetRef ref) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return SizedBox(
+        height: 200, // モーダルの高さ
+        child: GridView.count(
+          crossAxisCount: 5, // 1行あたりの色の数
+          children: paletteColors.map((color) {
+            return InkWell(
+              onTap: () async{
 
+                Navigator.pop(context);
+
+                 ref.read(colorSharedPreferencesProvider).setInt("color", color.value);
+
+                 ref.watch(colorProvider.notifier).update((state) => ref.watch(colorSharedPreferencesProvider).getInt("color")!);
+
+              },
+              child: Container(
+                margin: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: color,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      );
+    },
+  );
+}
+
+
+final List<Color> paletteColors = [
+  Colors.red,
+  Colors.green,
+  Colors.cyan,
+  Colors.orange,
+  Colors.purple,
+  // 他の色
+];
 
